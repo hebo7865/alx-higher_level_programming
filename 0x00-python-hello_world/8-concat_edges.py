b91193d8ec6d7aa8a14030ad59b90418fdea9a04
hebo7865 <lihf95@gmail.com>
2023-11-27 17:03:54 +0200

    concat edge

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(f"object-oriented {str[:str.index(',')]}")
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
