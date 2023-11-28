a52b66ab7c2155971469852c9537e7b5d7d42220
hebo7865 <lihf95@gmail.com>
2023-11-28 20:15:34 +0200

    comb

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(10):
+    for n in range(i, 10):
+        if i < n:
+            print("{:d}{:d}".format(i, n),
+                end="\n" if i == 8 and n == 9 else ", ")
