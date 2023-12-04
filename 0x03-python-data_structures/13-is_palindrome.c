cb8d53b38ad6317c4db467983972d510c2cdfd0e
hebo7865 <lihf95@gmail.com>
2023-12-04 19:16:31 +0200

    is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: head of linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+	{
+		return (1);
+	}
+	return (palind(head, *head));
+}
+
+/**
+ * palind - to check if palindrome
+ * @head: head of list
+ * @end: list end
+ * Return: always 0
+*/
+int palind(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+	{
+		return (-1);
+	}
+	if (palind(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
