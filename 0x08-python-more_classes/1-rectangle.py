f3179f267b4235efd56a4a63b0bc5292d877687c
hebo7865 <lihf95@gmail.com>
2024-01-04 17:37:38 +0200

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
     def height(self, value):
         """setter for private instance height"""
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
