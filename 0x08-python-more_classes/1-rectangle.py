aa03e76f8934be27fc50467517407c2b6bedfc68
hebo7865 <lihf95@gmail.com>
2024-01-04 16:15:15 +0200

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,12 +10,12 @@ class Rectangle:
         """intialize the rectangle"""
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """getter for private instance width"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """setter for private instance width"""
@@ -24,12 +24,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """getter for private instance height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """setter for private instance height"""
