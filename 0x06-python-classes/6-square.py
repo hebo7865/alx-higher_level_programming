3acf91405e745d7ad7cee9417b185f5abf5cd592
hebo7865 <lihf95@gmail.com>
2024-01-06 19:20:43 +0200

    square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,13 +53,13 @@ class Square:
     def position(self):
         """getter for private inistance position"""
         return self.__position
-    
+
     @position.setter
     def position(self, value):
         """setter for private inistance position"""
         if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(isinstance(num, int) for num in value) or
-            not all(num >= 0 for num in value)):
-                raise TypeError("position must be a tuple of 2 positive integers")
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
