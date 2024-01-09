8356dd80a5e39ab257c1b72f590f4fdff8804a39
hebo7865 <lihf95@gmail.com>
2024-01-10 00:05:14 +0200

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """Insert a line of txt to a file"""
     with open(filename, "r", encoding="utf-8") as f:
         listLine = []
         while True:
