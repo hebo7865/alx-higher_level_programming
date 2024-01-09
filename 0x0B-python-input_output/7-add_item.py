65208abc572c047dcd2db7a9202a5706d2deebb5
hebo7865 <lihf95@gmail.com>
2024-01-09 22:15:55 +0200

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for add_item"""
+
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = list(sys.argv[1:])
+
+try:
+    data = load_from_json_file('add_item.json')
+except:
+    data = []
+
+data.extend(args)
+save_to_json_file(data, 'add_item.json')
