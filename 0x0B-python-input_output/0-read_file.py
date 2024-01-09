2cfb3f6b26d9f88f67432f4286c213ee69d4808d
hebo7865 <lihf95@gmail.com>
2024-01-09 19:00:09 +0200

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module for read_file function"""
+
+
 def read_file(filename=""):
+    """Print file content to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read()[:-1])
