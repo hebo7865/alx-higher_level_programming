6ca9231b038fce09f6b4decb6644affcab9b5f50
hebo7865 <lihf95@gmail.com>
2024-01-09 23:17:23 +0200

    student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Define Student class"""
+
+
+class Student:
+    """
+    class represent a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialize the student data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the dictionary desciption for json"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        myDict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                myDict[key] = value
+        return myDict
+
+    def reload_from_json(self, json):
+        """replace all attributes of the student instance"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
+        
