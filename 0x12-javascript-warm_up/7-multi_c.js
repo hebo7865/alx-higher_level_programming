eb412046fa821ecfa83b35d9277674ac5e3e18fb
hebo7865 <lihf95@gmail.com>
2024-03-12 03:53:00 +0200

    object fct

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arg = process.argv[2]
+const arg = process.argv[2];
 
 if (isNaN(Number(arg))) {
   console.log('Missing number of occurrences');
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const arg = process.argv[2];
 if (isNaN(Number(arg))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < arg; i++) {
+  for (let i = 0; i < Number(arg); i++) {
     console.log('x'.repeat(arg));
   }
 }
