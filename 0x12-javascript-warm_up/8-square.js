1c345ab35ba7feac77a509ea3256a76918b48147
hebo7865 <lihf95@gmail.com>
2024-03-12 04:28:13 +0200

    object fct

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-const square = parseInt(arg);
+const square = Number(arg);
 
 if (isNaN(square) || square === undefined) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < square; i++) {
-    console.log('x'.repeat(square));
+    console.log('X'.repeat(square));
   }
 }
