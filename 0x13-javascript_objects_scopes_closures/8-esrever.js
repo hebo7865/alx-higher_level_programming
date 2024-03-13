2fdec8345e02e18bbfe2bb9d267e1772b4088012
hebo7865 <lihf95@gmail.com>
2024-03-13 05:34:25 +0200

    map

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   let len = list.length - 1;
   let i = 0;
-  while ((len - 1) > 0) {
+  while ((len - i) > 0) {
     const exch = list[len];
     list[len] = list[i];
     list[i] = exch;
