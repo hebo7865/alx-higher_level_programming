ba32747238b8c86880098134dec126a016a36ea8
hebo7865 <lihf95@gmail.com>
2024-03-13 04:52:01 +0200

    esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let len = list.length - 1;
-    let i = 0;
-    while ((len - 1) > 0) {
-        const exch = list[len];
-        list[len] = list[i];
-        list[i] = exch;
-        i++;
-        len--;
-    }
-    return list;
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - 1) > 0) {
+    const exch = list[len];
+    list[len] = list[i];
+    list[i] = exch;
+    i++;
+    len--;
+  }
+  return list;
 };
