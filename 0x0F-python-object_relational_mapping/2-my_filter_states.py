e27ea1491e29c6603a7c49b4eb25ce889dba956a
hebo7865 <lihf95@gmail.com>
2024-03-17 02:10:53 +0200

    filter state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa where name matches the argument
+list all states from the database hbtn_0e_0_usa where name matches the argument
 """
 
 
